7a93b08375e9154b0e33ebaaad9e5d1cd73125a8
kelvinmunene9839 <kelvinnyagah9839@gmail.com>
2026-07-09 08:46:27 +0200

    fixed uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{}".format(c), end="")
+        code = ord(c) - 32 if ord(c) >= 97 and ord(c) <= 122 else ord(c)
+        print("{:c}".format(code), end="")
     print("")
