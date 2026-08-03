98f781f5bdf651ae76eb328e726556c1e792f0cd
kelvinmunene9839 <kelvinnyagah9839@gmail.com>
2026-08-03 13:03:26 +0200

    fix

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""Locked class that only allows the first_name attribute."""
+
+
 class LockedClass:
+    """Prevent dynamic creation of instance attributes except first_name."""
+
     __slots__ = ["first_name"]
